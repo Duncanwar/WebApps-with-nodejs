36b1c6c89aeadac1ed0ea9159ad69a468b2f7628
Duncan <semunda9@gmail.com>
2020-04-09 12:04:37 +0200

    added eslint

--- app.js
+++ app.js
@@ -1,14 +1,25 @@
-var express = require("express");
-var chalk= require("chalk");
-var app= express();
- // to get route
-app.get("/" ,function(req, res){
-  res.send('Hello My Library App ');
+const express = require('express');
+const chalk = require('chalk');
+const debug = require('debug')('app');
+const morgan = require('morgan');
+const path = require('path');
+
+const app = express();
+
+app.use(morgan('tiny'));
+
+// serving static files
+
+app.use(express.static(path.join(__dirname, '/public/')));
+// app.use(express.static(path(__dirname,'/node_modules/bootstrap/dist/css')));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '/views/', '/index.html'));
 });
 
 
 // how to listen
 
-app.listen(3000,function(){
-console.log(`Sever has started ${chalk.gray('3000')}`);
+app.listen(3000, () => {
+  debug(`Sever has started ${chalk.gray('3000')}`);
 });
